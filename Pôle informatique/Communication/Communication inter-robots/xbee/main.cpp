d6b3b17b4189b28e4c69780a42cc23baf66790f8
Samuel-Charles DITTE-DESTREE <samueldittedestree@protonmail.com>
2022-03-01 14:57:01 +0100

    maj xbee

--- Pôle informatique/Communication/Communication inter-robots/xbee/main.cpp
+++ Pôle informatique/Communication/Communication inter-robots/xbee/main.cpp
@@ -6,13 +6,13 @@ int main(int argc, char *argv[]){
     
     XBee xbee;
 
-    int error_open_connection = xbee.openSerialConnection();
+    /*int error_open_connection = xbee.openSerialConnection();
 
     if(error_open_connection != 1)
         cout << ": Erreur de connexion à " << XB_SERIAL_PORT << " [Code erreur : " << error_open_connection << "]." << endl;
     else
         cout << ": Connexion ouverte avec succès sur le port \"" << XB_SERIAL_PORT << "\".\n" << endl;
-
+*/
     //int error_configuration = xbee.checkATConfig();
     
     //if(error_configuration == XB_AT_E_SUCCESS)
@@ -20,13 +20,6 @@ int main(int argc, char *argv[]){
     //else
       //  cout << "Configuration AT non réussie | [Code erreur : " << error_configuration << "]" << endl;
        
-
-    //char msg[] = {'c', 'o', 'u', 'c', 'o', 'u'};
-
-    //xbee.sendTrame(XB_ADR_ROBOT_02, XB_FCT_TEST_ALIVE, msg);
-
-    //string test = "21213191E63";
-
     //vector<int> test = {0x02, 0x12, 0X13, 0X1, 0XA, 0x1E, 0x63, 0x6F, 0x75, 0x63, 0x6F, 0x75, 0x9B, 0x56, 0x03};
 
     //int result = xbee.subTrame(test);
@@ -40,15 +33,11 @@ int main(int argc, char *argv[]){
 
     //xbee.subTrame(trame_totale);
     
-    thread heartbeat(&XBee::sendHeartbeat, xbee);
-    thread t(&XBee::waitForATrame, xbee);
-    while(true){}
+    //thread heartbeat(&XBee::sendHeartbeat, xbee);
+    //thread t(&XBee::waitForATrame, xbee);
+    //while(true){}
 
-    //t.join();
-
-    //xbee.sendMsg("coucou");
-
-    xbee.closeSerialConnection();
+    //xbee.closeSerialConnection();
     
     cout << ": Connexion fermée avec succès sur le port \"" << XB_SERIAL_PORT << "\".\n" << endl;
     return EXIT_SUCCESS;
