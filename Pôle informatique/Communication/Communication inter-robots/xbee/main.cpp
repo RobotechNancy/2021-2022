14241d87f43b82e0a8337f088bd5cef716fa0297
HeatSink Ru <heatsink.ru@gmail.com>
2022-03-15 11:14:20 +0100

    xbee maj

--- Pôle informatique/Communication/Communication inter-robots/xbee/main.cpp
+++ Pôle informatique/Communication/Communication inter-robots/xbee/main.cpp
@@ -11,7 +11,8 @@ int main(int argc, char *argv[]){
         return 0;
 
     thread heartbeat(&XBee::sendHeartbeat, xbee);
-    thread t(&XBee::waitForATrame, xbee);
+    thread waitingtrame(&XBee::waitForATrame, xbee);
+    thread reponse(&XBee::isXbeeResponding, xbee); 
     while(true){}
 
     xbee.closeSerialConnection();
