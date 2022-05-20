fdc2652d00f775f1579a9ce63ad231d1f704e157
Til160 <78447269+Til160@users.noreply.github.com>
2022-05-20 22:03:55 +0200

    Add files via upload

---
+++ Pôle informatique/Automate/Petit Robot/Debug/Core/Src/subdir.mk
@@ -0,0 +1,54 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/deplacementActionneur.c \
+../Core/Src/main.c \
+../Core/Src/motorActionneur.c \
+../Core/Src/servo.c \
+../Core/Src/stm32f3xx_hal_msp.c \
+../Core/Src/stm32f3xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32f3xx.c \
+../Core/Src/variableActionneur.c 
+
+C_DEPS += \
+./Core/Src/deplacementActionneur.d \
+./Core/Src/main.d \
+./Core/Src/motorActionneur.d \
+./Core/Src/servo.d \
+./Core/Src/stm32f3xx_hal_msp.d \
+./Core/Src/stm32f3xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32f3xx.d \
+./Core/Src/variableActionneur.d 
+
+OBJS += \
+./Core/Src/deplacementActionneur.o \
+./Core/Src/main.o \
+./Core/Src/motorActionneur.o \
+./Core/Src/servo.o \
+./Core/Src/stm32f3xx_hal_msp.o \
+./Core/Src/stm32f3xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32f3xx.o \
+./Core/Src/variableActionneur.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o Core/Src/%.su: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F303x8 -c -I../Core/Inc -I../Drivers/STM32F3xx_HAL_Driver/Inc -I../Drivers/STM32F3xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F3xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-Src
+
+clean-Core-2f-Src:
+	-$(RM) ./Core/Src/deplacementActionneur.d ./Core/Src/deplacementActionneur.o ./Core/Src/deplacementActionneur.su ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/motorActionneur.d ./Core/Src/motorActionneur.o ./Core/Src/motorActionneur.su ./Core/Src/servo.d ./Core/Src/servo.o ./Core/Src/servo.su ./Core/Src/stm32f3xx_hal_msp.d ./Core/Src/stm32f3xx_hal_msp.o ./Core/Src/stm32f3xx_hal_msp.su ./Core/Src/stm32f3xx_it.d ./Core/Src/stm32f3xx_it.o ./Core/Src/stm32f3xx_it.su ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f3xx.d ./Core/Src/system_stm32f3xx.o ./Core/Src/system_stm32f3xx.su ./Core/Src/variableActionneur.d ./Core/Src/variableActionneur.o ./Core/Src/variableActionneur.su
+
+.PHONY: clean-Core-2f-Src
+
