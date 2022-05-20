f17cf12bfef395117d62c103dc3dafc07b8cd5f0
Til160 <78447269+Til160@users.noreply.github.com>
2022-05-20 22:00:19 +0200

    Add files via upload

---
+++ Pôle informatique/Automate/Petit Robot/Core/Inc/variableActionneur.h
@@ -0,0 +1,66 @@
+/*
+ * variable.h
+ *
+ *  Created on: 8 mai 2022
+ *      Author: lucie
+ */
+
+#ifndef INC_VARIABLEACTIONNEUR_H_
+#define INC_VARIABLEACTIONNEUR_H_
+
+//#include "main.h"
+#include "stm32f3xx_hal.h"
+
+typedef enum {
+	ARRET = 0,
+	ACCELERATION = 1,
+	MAINTIENT,
+	DECELERATION
+} Deplacement_State_t;
+
+typedef enum{
+	NEUTRE = 0,
+	STATUETTE = 1,
+	AIMANT = 2,
+	INCONNUE = 3
+} Position_t;
+
+typedef struct{
+    int diviseur; //= freq_timer / freq_voulue, si =0 n'avance pas
+    int cnt;
+
+    uint16_t PIN_STEP; //STEP_Mx
+    uint16_t PIN_DIR; //DIR_Mx
+    uint16_t PIN_EN; //EN_Mx
+    GPIO_TypeDef* PIN_STEP_TYPE; //GPIOx
+    GPIO_TypeDef* PIN_DIR_TYPE; //GPIOx
+    GPIO_TypeDef* PIN_EN_TYPE; //GPIOx
+    int pinStepState;
+    int pinDirState;
+    int pinEnState;
+
+} Motor_t;
+
+//Fonctions
+uint32_t get_length(void);
+void set_length(uint32_t l);
+uint32_t* get_arr(void);
+uint32_t* get_r_arr(void);
+uint32_t get_tim_arr(void);
+Deplacement_State_t get_deplacement_state(void);
+void set_deplacement_state(Deplacement_State_t state);
+Position_t get_position(void);
+void set_position(Deplacement_State_t pos);
+Position_t get_next_position(void);
+void set_next_position(Deplacement_State_t pos);
+uint32_t get_target(void);
+void set_target(uint32_t t);
+void reset_steps(void);
+void count_steps(void);
+uint32_t get_steps(void);
+uint32_t get_steps_left(void);
+uint32_t get_freq(void);
+float time_left(void);
+
+
+#endif /* INC_VARIABLEACTIONNEUR_H_ */
